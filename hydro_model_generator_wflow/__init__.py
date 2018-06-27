e40681bb5315da08e53d0588dd60087e52a76978
Martijn Visser <mgvisser@gmail.com>
2018-06-27 17:38:50 +0200

    init import

--- hydro_model_generator_wflow/__init__.py
+++ hydro_model_generator_wflow/__init__.py
@@ -5,3 +5,5 @@
 __author__ = """Christine Rogers"""
 __email__ = 'rogersckw9@gmail.com'
 __version__ = '0.1.0'
+
+from hydro_model_generator_wflow.hydro_model_generator_wflow import ModelGeneratorWflow
